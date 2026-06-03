85e4d15e5412a7a88837128b279a8b3e93b39afd
viswas163 <viswas163@gmail.com>
2026-06-02 21:54:37 -0500

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        forwardProds = [1 for i in range(len(nums))]
+        reverseProds = [1 for i in range(len(nums))]
+        trailingProd = 1
+        prevNum = 1
+
+        for i, num in enumerate(nums):
+            forwardProds[i] = trailingProd * prevNum
+            trailingProd = forwardProds[i]
+            prevNum = num
+
+        trailingProd = 1
+        prevNum = 1
+        products = [1 for i in range(len(nums))]
+
+        for i, num in enumerate(reversed(nums)):
+            fwdIndex = len(nums) - i - 1
+            reverseProds[fwdIndex] = trailingProd * prevNum
+            trailingProd = reverseProds[fwdIndex]
+            prevNum = num
+            products[fwdIndex] = forwardProds[fwdIndex] * reverseProds[fwdIndex]
+
+        return products
